c695ef808a29176b26b535af352931dbe37e1013
Sri Sai Krishna Bathula <S562321@nwmissouri.edu>
2023-11-09 23:57:38 -0600

    change one of the productName and brand name

--- controllers/mobiles.js
+++ controllers/mobiles.js
@@ -1,63 +1,86 @@
 var mobile = require("../models/mobiles");
 // List of all mobiles
-exports.mobile_list = async function(req, res) {
-res.send('NOT IMPLEMENTED: mobile list');
+exports.mobile_list = async function (req, res) {
+  res.send('NOT IMPLEMENTED: mobile list');
 };
 // for a specific mobile.
-exports.mobile_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: mobile detail: ' + req.params.id);
+exports.mobile_detail = async function (req, res) {
+  console.log("detail" + req.params.id)
+  try {
+    result = await mobile.findById(req.params.id)
+    res.send(result)
+  } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+  }
 };
-// Handle Costume create on POST.
-exports.mobile_create_post = async function(req, res) {
+// Handle mobile create on POST.
+exports.mobile_create_post = async function (req, res) {
   console.log(req.body)
   let document = new mobile();
   // We are looking for a body, since POST does not have query parameters.
   // Even though bodies can be in many different formats, we will be picky
   // and require that it be a json object
-  // {"costume_type":"goat", "cost":12, "size":"large"}
+  // {"mobile_type":"goat", "cost":12, "size":"large"}
   document.productName = req.body.productName;
   document.brand = req.body.brand;
   document.price = req.body.price;
 
-  try{
-  let result = await document.save();
-  res.send(result);
+  try {
+    let result = await document.save();
+    res.send(result);
   }
-  catch(err){
-  res.status(500);
-  res.send(`{"error": ${err}}`);
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
   }
-  };
+};
 // Handle mobile delete form on DELETE.
-exports.mobile_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: mobile delete DELETE ' + req.params.id);
+exports.mobile_delete = function (req, res) {
+  res.send('NOT IMPLEMENTED: mobile delete DELETE ' + req.params.id);
 };
 // Handle mobile update form on PUT.
-exports.mobile_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: mobile update PUT' + req.params.id);
+exports.mobile_update_put = async function (req, res) {
+  console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+  try {
+    let toUpdate = await mobile.findById(req.params.id)
+    // Do updates of properties
+    if (req.body.productName)
+      toUpdate.productName = req.body.productName;
+    if (req.body.brand) toUpdate.brand = req.body.brand;
+    if (req.body.price) toUpdate.price = req.body.price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+  } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+  }
 };
 
 // List of all mobiles
-exports.mobile_list = async function(req, res) {
-try{
-themobile = await mobile.find();
-res.send(themobile);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.mobile_list = async function (req, res) {
+  try {
+    themobile = await mobile.find();
+    res.send(themobile);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 // VIEWS
 // Handle a show all view
-exports.mobile_view_all_Page = async function(req, res) {
-try{
-  themobile = await mobile.find();
-res.render('mobiles', { title: 'mobile Search Results', results: themobile });
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.mobile_view_all_Page = async function (req, res) {
+  try {
+    themobile = await mobile.find();
+    res.render('mobiles', { title: 'mobile Search Results', results: themobile });
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
--- routes/mobiles.js
+++ routes/mobiles.js
@@ -4,3 +4,6 @@ var router = express.Router();
 /* GET mobile */
 router.get('/', mobile_controlers.mobile_view_all_Page );
 module.exports = router;
+
+// GET request for one costume.
+router.get('/mobiles/:id', mobile_controlers.mobile_detail);
