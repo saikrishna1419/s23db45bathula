686ba8ffaaada8e887ca04d32ea2cf47a9777876
Sri Sai Krishna Bathula <S562321@nwmissouri.edu>
2023-11-15 18:22:16 -0600

    made few changes

--- controllers/mobiles.js
+++ controllers/mobiles.js
@@ -87,23 +87,18 @@ exports.mobile_list = async function (req, res) {
 };
 
 // Handle a show one view with id specified by query
-exports.mobile_view_one_Page = async function (req, res) {
+exports.mobile_view_one_Page = async function(req, res) {
   console.log("single view for id " + req.query.id)
-  try {
-    result = await mobile.findById(req.query.id)
-    if (!result) {
-      // Handle the case where the instance with the given ID doesn't exist
-      res.status(404).send(`No instance to delete`);
-      return;
-    }
-    res.render('Mobiledetail',
-      { title: 'Mobiles Detail', toShow: result });
+  try{
+  result = await mobile.findById( req.query.id)
+  res.render('mobiledetail',
+  { title: 'Mobile Detail', toShow: result });
   }
-  catch (err) {
-    res.status(500)
-    res.send(`{'error': '${err}'}`);
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
   }
-};
+  };
 
 // Handle building the view for creating a costume.
 // No body, no in path parameter, no query.
@@ -121,23 +116,19 @@ exports.mobile_create_Page = function (req, res) {
 
 // Handle building the view for updating a costume.
 // query provides the id
-exports.mobile_update_Page = async function (req, res) {
-  console.log("update view for item " + req.query.id)
-  try {
-    let result = await mobile.findById(req.query.id)
-    if (!result) {
-      // Handle the case where the instance with the given ID doesn't exist
-      res.status(404).send(`No instance to delete`);
-      return;
-    }
-    res.render('mobileupdate', { title: 'Mobile Update', toShow: result });
+exports.mobile_update_Page = async function(req, res) {
+  console.log("update view for item "+req.query.id)
+  try{
+  let result = await mobile.findById(req.query.id)
+  res.render('mobileupdate', { title: 'Mobile Update', toShow: result });
   }
-  catch (err) {
-    res.status(500)
-    res.send(`{'error': '${err}'}`);
+  catch(err){
+  res.status(500)
+  res.send(`{'error': '${err}'}`);
   }
-};
-
+  };
+ 
+  
 
 // Handle a delete one view with id from query
 exports.mobile_delete_Page = async function (req, res) {
